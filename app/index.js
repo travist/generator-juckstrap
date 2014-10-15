8cc0756c83f4740d22602c45d6c331fdf7819180
Travis Tidwell <codertravist@gmail.com>
2014-10-15 14:49:07 -0500

    Initial commit.

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
 'use strict';
 var util = require('util');
+var fs = require('fs');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
+var ncp = require('ncp').ncp;
+var async = require('async');
 
+// The Juckstrap Generator.
 var JuckstrapGenerator = yeoman.generators.Base.extend({
   initializing: function () {
     this.pkg = require('../package.json');
@@ -17,37 +21,85 @@ var JuckstrapGenerator = yeoman.generators.Base.extend({
       'Welcome to the super-excellent Juckstrap generator!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
-
+    // Prompt the user.
+    this.prompt([
+      {
+        type: 'input',
+        name: 'appTitle',
+        message: 'Application Title:',
+        default: 'My Application'
+      },
+      {
+        type: 'input',
+        name: 'analytics',
+        message: 'Google Analytics Account:',
+        default: 'UA-XXXXX-X'
+      },
+      {
+        type: 'input',
+        name: 'copyright',
+        message: 'Company name (for copyright message):',
+        default: 'My Company'
+      }
+    ], function (props) {
+      this.appTitle = props.appTitle;
+      this.analytics = props.analytics;
+      this.copyright = props.copyright;
       done();
     }.bind(this));
   },
 
   writing: {
     app: function () {
-      this.dest.mkdir('app');
-      this.dest.mkdir('app/templates');
+      var done = this.async();
+      var juckStrap = this.sourceRoot() + '/../../node_modules/juckstrap';
 
-      this.src.copy('_package.json', 'package.json');
-      this.src.copy('_bower.json', 'bower.json');
-    },
-
-    projectfiles: function () {
-      this.src.copy('editorconfig', '.editorconfig');
-      this.src.copy('jshintrc', '.jshintrc');
+      // Deep copy the juckstrap repo.
+      ncp(juckStrap, this.destinationRoot(), {
+        filter: function(source) {
+          var shouldFilter = !!source.match(/juckstrap[\/\\]node_modules/);
+          if (!shouldFilter) {
+              this.log('Copying ' + source);
+          }
+          return shouldFilter;
+        }.bind(this)
+      }, function doneCopying(err) {
+        if (err) return done(err);
+        var config = require(juckStrap + '/config.json');
+        config.locals.name = this.appTitle;
+        config.locals.analytics = this.analytics;
+        config.locals.copyright = this.copyright;
+        fs.unlinkSync(this.destinationRoot() + '/config.json');
+        fs.writeFile(this.destinationRoot() + '/config.json', JSON.stringify(config, null, 4), done);
+      }.bind(this));
     }
   },
 
   end: function () {
-    this.installDependencies();
+    async.parallel([
+      async.apply(this.npmInstall.bind(this), null, null),
+      async.apply(this.bowerInstall.bind(this), null, null),
+      function(done) {
+        this.spawnCommand('bower', ['install'], {
+          cwd: this.destinationRoot() + '/test'
+        })
+          .on('error', this.emit.bind(this, 'Bower install failed'))
+          .on('exit', this.emit.bind(this, 'bowerInstall:end'))
+          .on('exit', function (err) {
+            if (err === 127) {
+              this.log.error('Could not find bower. Please install with `npm install -g bower`.');
+            }
+            done();
+          }.bind(this));
+      }.bind(this)
+    ], function() {
+      this.spawnCommand('grunt', ['serve'])
+        .on('exit', function (err) {
+          if (err === 127) {
+            this.log.error('Could not find grunt. Please install with `npm install -g grunt-cli`.');
+          }
+        }.bind(this));
+    }.bind(this));
   }
 });
 
